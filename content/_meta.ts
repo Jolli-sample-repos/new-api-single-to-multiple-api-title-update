c2f1b02f5671aa27b57e0c927094e7b57b1d76f8
Aidan <230012360+aidancrosbie@users.noreply.github.com>
2025-12-11 18:01:31 -0800

    Update content/_meta.ts
    Updated via Jolli repository editor

--- content/_meta.ts
+++ content/_meta.ts
@@ -3,7 +3,7 @@ export default {
   'valid-troubleshooting': 'Valid - Troubleshooting',
   'valid-advanced-features': 'Valid - Advanced Features',
   'api-reference': {
-    title: 'API Reference',
+    title: 'Custom Title Moved To Menu',
     type: 'page',
     href: '/api-docs/valid-jolli-api-yaml'
   }
